db8e3330bab050dcab22865cf6a7c148dc6eee9b
MItsuki Shishido <169013117+dropmode@users.noreply.github.com>
2024-06-21 18:28:52 +0900

    質問①

---
+++ src/main/java/com/dmm/task/controller/EditController.java
@@ -0,0 +1,14 @@
+package com.dmm.task.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EditController {
+
+	@GetMapping("/testedit")
+	public String edit() {
+		return "edit";
+	}
+
+}
